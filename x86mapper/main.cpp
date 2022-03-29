5af95ee5e71285c98dfaf9675c3860f14a7bc2c2
jackzeraaaa <hangoverbicha@gmail.com>
2022-03-29 17:25:24 -0300

    comit seccond

--- x86mapper/main.cpp
+++ x86mapper/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	ManualMapper mapper;
 	char dllName[260];
-	std::cout << "Type dll name without \".dll\": ";
+	std::cout << "-> Type dll name without \".dll\": ";
 	std::cin >> dllName;
 	strcat_s(dllName, ".dll");
 	if(!mapper.InjectDll("csgo.exe", dllName))
